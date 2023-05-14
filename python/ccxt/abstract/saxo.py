0a2563035248ec1baf869158f19abb637f7b3ccd
jothi-devhub <jothi.murugan_92@live.com>
2023-05-14 13:54:52 +0530

    Created place order using openapi

--- python/ccxt/abstract/saxo.py
+++ python/ccxt/abstract/saxo.py
@@ -3,4 +3,6 @@ from ccxt.base.types import Entry
 
 class ImplicitAPI:
     
-    Accounthistory
+    #Entry def __init__(self, path, api, method, config):
+    
+    Post_place_Order = PostPlaceOrder = Entry('trade/v2/orders', 'openapi' , 'POST' ,{'cost':1})
