35657fbebe7f6daf06f07fbc4c30c9e0d4fea495
Jothimurugan Muthusamy <131227925+jothi-devhub@users.noreply.github.com>
2023-05-16 14:16:39 +0530

    Update saxo.py
    Added New functions  to ImplicitAPI class

--- python/ccxt/abstract/saxo.py
+++ python/ccxt/abstract/saxo.py
@@ -5,10 +5,12 @@ class ImplicitAPI:
         
     #Entry def __init__(self, path, api, method, config):
     Account_Values = AccountValues = Entry('openapi/hist/v3/accountValues/{ClientKey}/', 'openapi', 'GET', {'cost':1})
-    info_price_Specific_instrument = infopriceSpecificinstrument = Entry('openapi/trade/v1/infoprices', 'openapi', 'GET', {'cost':1)
-    info_price_list_instrument = infopricelistinstrument = Entry('openapi/trade/v1/infoprices/list', 'openapi', 'GET', {'cost':1})                                                                                   
+    Info_price_Specific_instrument = infopriceSpecificinstrument = Entry('openapi/trade/v1/infoprices', 'openapi', 'GET', {'cost':1)
+    Info_price_list_instrument = infopricelistinstrument = Entry('openapi/trade/v1/infoprices/list', 'openapi', 'GET', {'cost':1})                                                                                   
     Info_Price_Subscription = InfoPriceSubscription = Entry('openapi/trade/v1/infoprices/subscriptions', 'openapi', 'POST', {'cost':1})
     Remove_Info_Price_Subscriptions_By_Tag = RemoveInfoPriceSubscriptionsByTag = Entry ('openapi/trade/v1/infoprices/subscriptions/{ContextId}', 'openapi', 'DELETE', {'cost':202})
-    Remove_Info_Price_Subscription_By_Id = RemoveInfoPriceSubscriptionById = Entry ('openapi/trade/v1/infoprices/subscriptions/{ContextId}/{ReferenceId}', 'openapi', 'DELETE', {'cost':202})
-                                                                                                                           
-    Post_place_Order = PostPlaceOrder = Entry('openapi/trade/v2/orders', 'openapi' , 'POST' ,{'cost':1})
+    Remove_Info_Price_Subscription_By_Id = RemoveInfoPriceSubscriptionById = Entry ('openapi/trade/v1/infoprices/subscriptions/{ContextId}/{ReferenceId}', 'openapi', 'DELETE', {'cost':202}                                                                                                                   
+    Place_New_Order = PlaceNewOrder = Entry('openapi/trade/v2/orders', 'openapi' , 'POST' ,{'cost':1})
+    Change_Order = ChangeOrder = Entry('openapi/trade/v2/orders', 'openapi' , 'PATCH' ,{'cost':1})
+    Cancel_Order = CancelOrder = Entry('openapi/trade/v2/orders', 'openapi' , 'DELETE' ,{'cost':1})
+    
